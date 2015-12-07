9e4173f73bfd1a203d12dea036fd7082ee0a1d68
松本 泰典 <matsumoto@caters.co.jp>
2015-12-07 21:06:59 +0900

    [ modify ] string : directory
    setup.

--- src/assets/js/ex/String/ReplaceURL.ts
+++ src/assets/js/ex/String/ReplaceURL.ts
@@ -1,5 +1,4 @@
 ReplaceURL = (_text:string):string => {
   return _text.replace(/((http|https|ftp):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/g, '<a href="$1" target="_blank">$1</a>'));
 }
-
 export = ReplaceURL;
--- src/assets/js/ex/String/dateToStr.ts
+++ src/assets/js/ex/String/dateToStr.ts
@@ -2,5 +2,4 @@ DateToStr = (_date:string, _seperlator:string = '.'):string => {
   var _str = _date.getFullYear() + _seperlator + (_date.getMonth()+1) + _seperlator + _date.getDate();
   return _str;
 }
-
 export = DateToStr;
